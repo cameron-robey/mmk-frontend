7afa26235c46ee4b99e41d630bba394333217914
Cameron Robey <cameron@cameronrobey.co.uk>
2020-09-13 05:04:38 +0100

    fix bug with pathname of deployment for server

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const port = 4500;
 
 app.use(express.static(path.join(__dirname, '../build')));
 app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
 app.listen(port, () => {
